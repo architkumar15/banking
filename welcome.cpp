32c220b67730cdefe21b7dbb2a29cd86719d25f4
archit kumar <68789686+architkumar15@users.noreply.github.com>
2020-09-01 16:10:14 +0530

    Create welcome.cpp

---
+++ welcome.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<conio.h>
+#include<windows.h>
+#include<unistd.h>
+using namespace std;
+void welcome();
+void me();
+ /*int main()
+ {  
+    welcome();   
+    getch();
+    return 0;
+  } 
+  */
+  void welcome()
+  {
+  cout<<"\n\n\n\n\n\n\n ";
+  system("color C");
+  cout<<"\t\t\t\t\t\t      	WELCOME    "<<endl;
+  cout<<endl;
+  cout<<"\t\t\t\t\t\t      TO           "<<endl;
+  cout<<endl<<endl<<endl<<endl<<endl;
+  me();
+    }
+ 
+ void me()
+ {
+  cout<<"\t\t\t\t\t    BANKING SYSTEM          "<<endl;
+   
+ }
+  
+  
